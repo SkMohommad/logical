c150f46486e4536c23e0456f5629773d5c921545
SkMohammad <80616052+SkMohommad@users.noreply.github.com>
2023-04-09 16:43:14 +0530

    Add files via upload

---
+++ src/com/sk/logic/BinarySearch.java
@@ -0,0 +1,35 @@
+package com.sk.logic;
+
+public class BinarySearch {
+
+		public static int binarySearch(int[] arr, int key) {
+			int left = 0;
+			int right = arr.length - 1;
+
+			while (left <= right) {
+				int mid = left + (right - left) / 2;
+				if (arr[mid] == key) {
+					return mid;
+				} else if (arr[mid] < key) {
+					left = mid + 1;
+				} else {
+					right = mid - 1;
+				}
+			}
+
+			return -1;
+		}
+
+		public static void main(String[] args) {
+			int[] arr = { 1, 2, 3, 4, 5 };
+			int key = 4;
+
+			int result = binarySearch(arr, key);
+
+			if (result == -1) {
+				System.out.println("Element not found");
+			} else {
+				System.out.println("Element found at index " + result);
+			}
+		}
+	}
